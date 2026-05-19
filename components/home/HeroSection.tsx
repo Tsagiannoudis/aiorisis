6204475342ded4089aa90ab9d8a683080bf12434
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2026-05-20 00:55:03 +0300

    remove bg  at the first section

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -20,7 +20,7 @@ useEffect(() => {
 
   return (
     <>
-      <section className="relative z-10 text-gray-900 pt-2 md:pt-1 pb-4 md:pb- bg-[#F5F5F5]">
+      <section className="relative z-10 text-gray-900 pt-2 md:pt-1 pb-4 md:pb-8">
         <div className="container mx-auto px-4 max-w-6xl overflow-hidden">
           <div className="font-[family-name:var(--font-ubuntu-sans)] opacity-60">
             <h1
